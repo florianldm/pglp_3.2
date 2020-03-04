923fcb169416e9a651893bcc93374c6a29dc1795
= <ledeme.florian@gmail.com>
2020-03-04 14:03:38 +0100

    Ajout test employe

---
+++ src/test/java/fr/uvsq/florianldm/EmployeTest.java
@@ -0,0 +1,21 @@
+package fr.uvsq.florianldm;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeTest {
+    Employe E;
+    @Before
+    public void initEmploye(){
+        E = new Employe("Edouard", 15);
+    }
+    @Test
+    public void testInstance(){
+        Assert.assertTrue(E.getNom().equals("Edouard") && E.getAnciennete() == 15);
+    }
+
+    @Test
+    public void testSalaire(){
+        Assert.assertTrue(E.calculSalaire() == 15 * E.salaire);
+    }
+}
